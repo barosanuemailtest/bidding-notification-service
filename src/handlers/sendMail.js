329012b39fe495a9b53fddb9fe1c2bf137f17ab8
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-07 12:02:01 +0200

    MailQueue

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,20 +3,26 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'eu-west-1' });
 
 
-async function sendMail(event, context) {
+
+async function sendMail(event, context) {  
+  const record = event.Records[0];
+  console.log('record processing: ', record);
+  const email = JSON.parse(record.body)
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'barosanu2489@gmail.com',
     Destination: {
-      ToAddresses: ['barosanu2489@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from AWS SES!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
